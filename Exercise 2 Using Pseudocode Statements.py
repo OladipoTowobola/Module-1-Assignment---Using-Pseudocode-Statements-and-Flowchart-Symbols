1444e9650fc8deed94a08a7e28b2165de4c7ffbe
OladipoTowobola <otowobola@ivytech.edu>
2024-08-27 14:22:28 -0400

    Add files via upload

---
+++ Exercise 2 Using Pseudocode Statements.py
@@ -0,0 +1,20 @@
+Exercise 2
+// This pseudocode is intended to describe
+// computing the daily cost of your rent
+// in a 30-day month
+
+
+Start
+   Input monthlyRent
+   dailyRent = monthlyRent / 30
+   Output dailyRent
+
+Stop
+
+
+   //Comments
+   Start //Consistently found at the commencement of our pseudocode.
+   Input // User  prompted to enter total monthlyRent.
+   dailyRent // Calculates dailyRent by dividing monthlyRent by 30-day month.
+   Output dailyRent // Displays  Calculated dailyRent to user
+   Stop //Process Ends
---
+++ Exercise 3 Using Pseudocode Statements.py
@@ -0,0 +1,25 @@
+Exercise 3
+// This program accepts a user's monthly pay
+// and rent, utilities, and grocery bills
+// and displays the amount available for discretionary spending
+// (which might be negative)
+// Your program should output the pay and the total bills
+// as well as the remaining discretionary amount
+
+
+
+Start
+  Input monthlyPay,rent,utilities,grocery_bills
+  TotalBills = rent + utilities + grocery_bills
+  DiscretionarySpending = monthlyPay - TotalBills
+  Output monthlyPay,TotalBills,DiscretionarySpending
+  Stop
+  
+
+//Comments
+   Start //Consistently found at the commencement of our pseudocode
+   Input monthlypay,rent,utilities,grocery_bills //Prompted User to enter
+   totalbills = rent + utilities + grocery_bills // Calculate total bills by adding rent, utilities, and grocery bills
+   discretionary spending //Calculate monthlypay spending by substracting from total bills
+   Output // Displays the user's monthly pay,total bills and discretionary spending
+   Stop //Process Ends
