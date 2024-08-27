8ded0e5ee837ad6fa605c33eae3aa4c80304034f
OladipoTowobola <otowobola@ivytech.edu>
2024-08-27 14:20:19 -0400

    Add files via upload

---
+++ Exercise 1 Using Pseudocode Statements.py
@@ -0,0 +1,18 @@
+Exercise 1
+// This pseudocode is intended to compute the number
+// of miles per gallon you get with your automobile
+
+Start
+   Input BeginOdometer, EndOdometer, GallonUsed
+   DistanceDriven = EndOdometer - BeginOdometer
+   MilesPerGallon = DistanceDriven /  GallonUsed
+   Output MilesPerGallon
+Stop
+
+   //Comments
+   Start //Consistently found at the commencement of our pseudocode
+   Input // User  prompted to enter BeginOdometer readings, EndOdometer readings, GallonUsed
+   DistanceDriven // Calculates the differences between the EndOdometer readings and BeginOdometer readings
+   MilesPerGallon // Calculates the DistanceDriven divided by GallonUsed to determine the  MilesPerGallon
+   Output // Displays the calculated MilesPerGallon
+   Stop //Process Ends
